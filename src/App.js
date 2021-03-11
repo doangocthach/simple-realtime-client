b2b91d72e0b6a7fb9b676c9d07f5a84174918c2c
Doan Ngoc Thach <thachdn@ftech.ai>
2021-03-11 09:38:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import socketIOClient from "socket.io-client";
+import { useEffect, useRef, useState } from 'react';
+const socket = socketIOClient("http://localhost:8080/");
 function App() {
+  const ref = useRef(false);
+  const [state, setstate] = useState([]);
+  const [msg, setMsg] = useState('');
+  useEffect(() => {
+    if (ref.current === false) {
+      socket.emit("show_message");
+      socket.on("get_data", data => getData(data));
+      ref.current = true;
+    }
+    return () => {
+      socket.off("show_message")
+      socket.off("add_message")
+    }
+  }, [])
+  const getData = data => {
+    setstate(data)
+  }
+  const sendData = () => {
+    socket.emit("add_message", msg)
+    socket.emit("show_message");
+    setMsg("")
+  }
+  const changeText = e => {
+    setMsg(e.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={msg} onChange={changeText} />
+      <button onClick={sendData}>send</button>
+      <ul>
+        {
+          state.map(e => {
+            return <li>{e.msg}</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
